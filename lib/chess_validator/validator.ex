b727908c5d06ecd3f576b7a8a68dcb56da1bbbe0
Ludo van den Boom <ludo@cubicphuse.nl>
2016-01-22 11:18:58 +0100

    Prettify

--- lib/chess_validator/validator.ex
+++ lib/chess_validator/validator.ex
@@ -22,9 +22,7 @@ defmodule ChessValidator.Validator do
   def process_fields([ head | tail ], accumulator) do
     process_fields(tail, accumulator ++ [head |> String.strip |> process_field])
   end
-  def process_fields([], accumulator) do
-    accumulator
-  end
+  def process_fields([], accumulator), do: accumulator
 
   def process_field(field) do
     if field == "--", do: nil, else: String.to_atom(field)
